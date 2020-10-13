e67e67375b156549258bb7f6be6c20035ba778ab
Alexander Bowers <7319abowers@gmail.com>
2020-10-13 05:46:09 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,4 +3,13 @@ def find_even_values(src)
   #
   # Output all even values in each nested array
 
+src.each do |even|
+  even.each do |inner_even|
+    if inner_even % 2 == 0
+      p inner_even
+    end
+
+  end
+end
+
 end
